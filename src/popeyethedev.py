fbd20cec30c67ad52ca8e09867faa6ac7f8bf97d
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-06 13:58:09 +0530

    Create popeyethedev.py

---
+++ src/popeyethedev.py
@@ -0,0 +1,15 @@
+# popeye the dev
+
+@app.route('/developer')
+def pope():
+    return f"""
+    popeye
+    
+    the developer
+    
+    skills: programming paradigms, technology
+    
+    10 YoE - progeramming, web development,
+        server/client communication,
+        app development, XSS, cors, ...
+    """
