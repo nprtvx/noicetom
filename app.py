48cac887b469bcb0bea3fc05997ca9327578f7aa
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-06 14:07:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 from pathlib import Path
-#from src import popeyethedev as popeye
+from src import popeyethedev as popeye
 
 app = Flask(__name__, template_folder=Path("./templates"))
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/developer')
+def pope():
+  return popeye.pope
 
 if __name__=='__main__':
     app.run(debug=False)
