360a438f1d898e06c0f44d947b7a5c7d92ec35e6
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-06 13:31:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder=Path("./templates"))
 
 @app.route('/')
 def index():
-  return render_template(app.template_folder, 'index.html')
+  return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=False)
