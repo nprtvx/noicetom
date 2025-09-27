0386296fd82bfbd9cfc0f6bff18bf34824ff3fcd
nprtvx <nprtvx@users.noreply.github.com>
2025-09-27 16:35:25 +0000

    deploy: f0bad91af6fd811974f1ca0b078fd63b561e6c9f

--- static/js/index.js
+++ static/js/index.js
@@ -7,3 +7,16 @@ popeye.addEventListener('click', () => {
     popeye.style.display = 'flex';
 });
 document.body.append(popeye);
+
+const nprtvx = document.getElementById("nprtvx");
+
+nprtvx.addEventListener("click", () => {
+    nprtvx.style.backgroundColor = "#26111992";
+    nprtvx.style.width = "26px";
+    nprtvx.style.height = "26px";
+    nprtvx.style.border = "2px solid";
+    nprtvx.style.borderRadius = "26px";
+});
+
+
+    
